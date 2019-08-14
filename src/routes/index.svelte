80f4e9865a832b2ff6c9ed8a78216bd66a40791f
Wei Chen <wchen165@me.ema.ad.pwcinternal.com>
2019-08-14 12:52:05 +0400

    added SCSS preprocessor

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,55 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+<style lang="scss">
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+    @media only screen and (min-width: 480px) {
+      font-size: 4em;
+    }
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  figure {
+    margin: 0 0 1em 0;
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
 
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  p {
+    margin: 1em auto;
+  }
 </style>
 
 <svelte:head>
-	<title>vcheeze's home</title>
+  <title>vcheeze's home</title>
 </svelte:head>
 
 <h1>vcheeze's home!</h1>
 
 <figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>AWESOME</figcaption>
+  <img alt="Borat" src="great-success.png" />
+  <figcaption>AWESOME</figcaption>
 </figure>
 
-<p><strong>You've landed on the home page of <a href="https://github.com/vcheeze">vcheeze</a>.</strong></p>
 <p>
-	It's still being built right now...so take a look at what you can see and
-	anticipate what you can't see just yet!
+  <strong>
+    You've landed on the home page of
+    <a href="https://github.com/vcheeze">vcheeze</a>
+    .
+  </strong>
+</p>
+<p>
+  It's still being built right now... so take a look at what you can see and
+  anticipate what you can't see just yet!
 </p>
